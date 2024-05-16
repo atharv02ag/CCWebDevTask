8bb38374c8b574609810f9c8ce99db7bc7f42e8f
atharv02ag <atharvagarwal2718@gmail.com>
2024-05-16 18:14:22 +0530

    upto task1

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-// const url = 'https://coding-week-2024-api.onrender.com/api/data';
+const url = 'https://coding-week-2024-api.onrender.com/api/data';
 
-// const getJSON = async (url)=>{
-//     const response = await fetch(url);
-//     const data = response.json();
-//     return data;
+const getJSON = async (url)=>{
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+}
+
+const nav = document.querySelector('nav');
+const sideHeading = document.querySelector('.side-bar .side-heading');
+nav.addEventListener("click",(e)=>{
+    for(let child of nav.children){
+        if(child.matches('.selected-nav')){
+            child.classList.remove('selected-nav');
+        }
+    }
+    e.target.classList.add('selected-nav');
+})
+
+sideHeading.addEventListener("click",(e)=>{
+    for(let child of sideHeading.children){
+        if(child.matches('.selected-side')){
+            child.classList.remove('selected-side');
+        }
+    }
+    e.target.classList.add('selected-side');
+})
+
+// const majorOne = document.querySelector('.major.one');
+// console.log(majorOne.children[0].children);
+
+// const setSection1 = (obj,element)=>{
 // }
-// getJSON(url).then(e=>{
-//     console.log(e);
+// getJSON('./api.json').then(e=>{
+//     console.log(e[0]);
 // })
+
