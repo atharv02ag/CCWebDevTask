30703d184b9476155cfee87a946d662cbcd2de39
atharv02ag <atharvagarwal2718@gmail.com>
2024-05-16 23:39:33 +0530

    section2 added

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const majorOne = document.querySelector('.major.one');
 const mediumTwo = document.querySelector('.medium.two');
 const minorThree = document.querySelector('.minor.three');
 const minorFour = document.querySelector('.minor.four');
+const sideArticles = document.querySelector('.side-articles');
 
 const months = {
     "01":"January",
@@ -53,6 +54,10 @@ const convDate = (date)=>{
     return (month+" "+day+", " + year);
 }
 
+const getRandom=(min, max)=>{
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 const setSection1 = (obj,element,index)=>{
     const newtype = document.createElement('span');
     newtype.classList.add('type');
@@ -69,11 +74,51 @@ const setSection1 = (obj,element,index)=>{
     const imgURL = obj[index].image;
     element.parentElement.style.backgroundImage = `url('${imgURL}')`;
 }
+
+const setSection2 = (obj,element,index)=>{
+    const newli = document.createElement('li');
+    newli.classList.add('side-article');
+    const sideImg = document.createElement('img');
+    sideImg.classList.add('side-img');
+    const imgURL = obj[index].image;
+    sideImg.src = imgURL;
+    newli.appendChild(sideImg);
+
+    const sideText = document.createElement('div');
+    sideText.classList.add('side-text');
+    const sideHeadline = document.createElement('p');
+    sideHeadline.classList.add('side-headline');
+    let headline = obj[index].headline;
+    if(headline.length > 45){
+        headline = headline.substring(0,45);
+        headline += '..';
+    }
+    sideHeadline.innerText = headline;
+    sideText.appendChild(sideHeadline);
+
+    const releaseDate = document.createElement('div');
+    releaseDate.classList.add('release-date');
+    const calender = document.createElement('img');
+    calender.classList.add('calender');
+    calender.src='calender.svg';
+    releaseDate.appendChild(calender);
+
+    const tareek = document.createElement('span');
+    tareek.innerText = convDate(obj[index].date);
+    releaseDate.appendChild(tareek);
+
+    sideText.appendChild(releaseDate);
+    newli.appendChild(sideText);
+    element.appendChild(newli);
+}
 getJSON('./api.json').then(e=>{
     setSection1(e,majorOne.children[0],0);
     setSection1(e,mediumTwo.children[0],1);
     setSection1(e,minorThree.children[0],2);
     setSection1(e,minorFour.children[0],3);
+    for(let i = 4; i<e.length; i++){
+        setSection2(e,sideArticles,i);
+    } 
     //console.log(e[0]);
 })
 
