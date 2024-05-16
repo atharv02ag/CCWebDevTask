8867a7c8e0369e177cbec32549a1ab05a45283ad
atharv02ag <atharvagarwal2718@gmail.com>
2024-05-16 13:00:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const url = 'https://coding-week-2024-api.onrender.com/api/data';
+
+// const getJSON = async (url)=>{
+//     const response = await fetch(url);
+//     const data = response.json();
+//     return data;
+// }
+// getJSON(url).then(e=>{
+//     console.log(e);
+// })
