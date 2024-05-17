77288e4e56997019f5464b3b268ae4f5d8bd3d3e
atharv02ag <atharvagarwal2718@gmail.com>
2024-05-17 13:51:30 +0530

    content box added

--- script.js
+++ script.js
@@ -32,6 +32,8 @@ const mediumTwo = document.querySelector('.medium.two');
 const minorThree = document.querySelector('.minor.three');
 const minorFour = document.querySelector('.minor.four');
 const sideArticles = document.querySelector('.side-articles');
+const article = document.querySelector('.article');
+const closeButton = document.querySelector('.article img');
 
 const months = {
     "01":"January",
@@ -59,30 +61,31 @@ const getRandom=(min, max)=>{
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const setSection1 = (obj,element,index)=>{
+const setSection1 = (obj,element,id)=>{
     const newtype = document.createElement('span');
     newtype.classList.add('type');
-    newtype.innerText = obj[index].type;
+    newtype.innerText = obj[id-1].type;
     element.insertBefore(newtype,element.children[0]);
-    let headline = obj[index].headline;
+    let headline = obj[id-1].headline;
     if(headline.length > 35){
         headline = headline.substring(0,35);
         headline += '..';
     }
     element.children[2].innerText = headline;
-    element.children[3].innerText = obj[index].author;
-    element.children[4].children[1].innerText = convDate(obj[index].date);
-    const imgURL = obj[index].image;
+    element.children[3].innerText = obj[id-1].author;
+    element.children[4].children[1].innerText = convDate(obj[id-1].date);
+    const imgURL = obj[id-1].image;
     element.parentElement.style.backgroundImage = `url('${imgURL}')`;
+    element.parentElement.setAttribute('data-id',id);
 }
 
-const setSection2 = (obj,element,index)=>{
+const setSection2 = (obj,element,id)=>{
     const newli = document.createElement('li');
     newli.classList.add('side-article');
     newli.classList.add('pop-anim-side');
     const sideImg = document.createElement('img');
     sideImg.classList.add('side-img');
-    const imgURL = obj[index].image;
+    const imgURL = obj[id-1].image;
     sideImg.src = imgURL;
     newli.appendChild(sideImg);
 
@@ -90,7 +93,7 @@ const setSection2 = (obj,element,index)=>{
     sideText.classList.add('side-text');
     const sideHeadline = document.createElement('p');
     sideHeadline.classList.add('side-headline');
-    let headline = obj[index].headline;
+    let headline = obj[id-1].headline;
     if(headline.length > 45){
         headline = headline.substring(0,45);
         headline += '..';
@@ -106,20 +109,61 @@ const setSection2 = (obj,element,index)=>{
     releaseDate.appendChild(calender);
 
     const tareek = document.createElement('span');
-    tareek.innerText = convDate(obj[index].date);
+    tareek.innerText = convDate(obj[id-1].date);
     releaseDate.appendChild(tareek);
 
     sideText.appendChild(releaseDate);
     newli.appendChild(sideText);
+    newli.setAttribute('data-id',id);
     element.appendChild(newli);
 }
-getJSON('./api.json').then(e=>{
-    setSection1(e,majorOne.children[0],0);
-    setSection1(e,mediumTwo.children[0],1);
-    setSection1(e,minorThree.children[0],2);
-    setSection1(e,minorFour.children[0],3);
-    for(let i = 4; i<e.length; i++){
-        setSection2(e,sideArticles,i);
+
+const writeArticle = (obj,id,posX,posY,side)=>{
+    article.children[1].innerText = obj[id-1].content;
+    if(side==true){
+        article.style.left = `${posX}px`;
+        article.style.top = `${posY+window.scrollY}px`;
+    }
+    else{
+        article.style.left = `${posX}px`;
+        article.style.top = `${posY+window.scrollY}px`;
+    }  
+}
+
+window.addEventListener("load",()=>{
+    article.style.opacity = 0;
+})
+
+closeButton.addEventListener('click',()=>{
+    article.style.opacity = 0;
+})
+
+getJSON('./api.json').then(data=>{
+    setSection1(data,majorOne.children[0],1);
+    setSection1(data,mediumTwo.children[0],2);
+    setSection1(data,minorThree.children[0],3);
+    setSection1(data,minorFour.children[0],4);
+    for(let i = 4; i<data.length; i++){
+        setSection2(data,sideArticles,i);
     } 
+    const content = document.querySelector('.content');
+    content.addEventListener('click',(e)=>{
+        for(let i = 1; i<=data.length; i++){
+            if(e.target.closest(`[data-id="${i}"]`) != null){
+                article.style.opacity = 1;
+                writeArticle(data,i,e.x,e.y,false);      
+           }
+        }      
+    })
+    sideArticles.addEventListener('click',(e)=>{
+        for(let i = 1; i<=data.length; i++){
+            if(e.target.closest(`[data-id="${i}"]`) != null){
+                article.style.opacity = 1;
+                const posX = e.x-article.offsetWidth;
+                writeArticle(data,i,posX,e.y,true);      
+           }
+        }   
+    })
+    
 })
 
